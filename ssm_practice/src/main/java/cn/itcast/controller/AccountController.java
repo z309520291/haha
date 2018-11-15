852da886dd858acb685c715a36be46539dda2ba2
z309520291 <309520291@qq.com>
2018-11-15 10:45:26 +0800

    test

---
+++ ssm_practice/src/main/java/cn/itcast/controller/AccountController.java
@@ -0,0 +1,45 @@
+package cn.itcast.controller;
+
+import cn.itcast.domain.Account;
+import cn.itcast.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 帐户web
+ */
+@Controller
+@RequestMapping("account")
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @RequestMapping("findAll")
+    public String findAll(Model model){
+        System.out.println("表现层：查询所有账户...");
+        // 调用service的方法
+        List<Account> list = accountService.findAll();
+        model.addAttribute("list",list);
+        return "list";
+    }
+
+    /**
+     * 保存
+     * @return
+     */
+    @RequestMapping("save")
+    public String save(Account account) throws IOException {
+        System.out.println("表现层：保存账户...");
+        accountService.saveAccount(account);
+        return "list";
+    }
+
+
+
+}
